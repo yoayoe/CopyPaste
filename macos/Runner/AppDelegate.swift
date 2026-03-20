1f3825e485534dd25d1e6e3bde1fcb08f7a7a04d
Yoyok Widiatmoko <widiatmoko.yoyok@pln.co.id>
2026-03-20 13:00:40 +0700

    fixing flutter run on macos

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,6 +3,11 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    super.applicationDidFinishLaunching(notification)
+    NSApp.activate(ignoringOtherApps: true)
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
